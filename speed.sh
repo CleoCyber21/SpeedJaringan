6b3a1923ab5e93966a379d6ef0e0e885fff766bc
andrabarokah <58820168+andrabarokah@users.noreply.github.com>
2019-12-12 23:26:53 +0800

    SpeedJaringan
    $pkg update && pkg upgrade
    $pkg install git

---
+++ speed.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Kak tutorial
+echo "'\033[33;1m'SELAMAT DATANG DI TOOLS KAK TUTORIAL"
+echo ""
+echo ""
+sleep 1
+echo "'\033[32;1m'JANGAN LUPA SUBSCRIBE CHANNEL KAK TUTORIAL"
+echo ""
+sleep 1
+echo "'\033[34;1m'INI TOOLS CEPATKAN JARINGAN"
+sleep 2
+clear
+
+
+echo "'\033[32;1m'HALLO PILIH YA"
+echo ""
+sleep 1
+echo "_-_-_-_-_-_-_-_-"
+echo ""
+sleep 1
+echo "    (1) LOGIN (langsung login aja)"
+echo "    (2) No wa gua"
+echo "    (3) EXIT "
+read -p "SILAKAN PILIH YA TEMAN TEMAN : " nomor
+if [ $nomor = "1" ]
+then
+
+clear
+echo "'\033[32;1m'yang udah tau jangan di publikasi kan pass nya ya"
+echo "SILAKAN TULIS DISINI "
+echo " username = subscribe , password = kak tutorial"
+sleep 1
+nama="subscribe"
+while [ "$siapa" != "$nama" ]
+do
+read -p "Apa username nya : " siapa
+done
+nama="kak tutorial"
+while [ "$siapa" != "$nama" ]
+do
+read -p "Apa pass nya : " siapa
+done
+
+clear
+
+elif [ $nomor = "2" ]
+then
+sleep 2
+echo "tanya aja"
+echo "tanya di wa ku"
+echo " 0895376666955"
+sleep 4
+clear
+elif [ $nomor = "3" ]
+then
+toilet -f big -F gay EXIT
+exit
+fi
+
+ulang="y"
+while [ $ulang = "y" ];
+do
+
+echo "'\033[35;1m'HALLO SAUDARA TUTORIAL"
+sleep 1
+echo "TINGGAL PILIH YA SESUAI KEBUTUHAN"
+echo "__________________________________"
+echo ""
+sleep 1
+echo "(1) MULAI"
+echo "(2) join grup wa"
+echo "(3) EXIT"
+read -p "PILIH NOMOR DI ATAS INI : " pilih
+if [ $pilih = "1" ]
+then
+clear
+toilet -f big -F gay kak tutorial
+echo $green "Cepatkan Jaringan By Kak Tutorial"
+ping -s9000 1.1.1.1
+
+elif [ $pilih = "2" ]
+then
+echo "sabar"
+echo "https://chat.whatsapp.com/DZyYrgnx0NvLQLwqGbEXSi"
+echo "salin tuh"
+echo "udah kan"
+sleep 2
+elif [ $pilih = "2" ]
+then
+toilet -f big -F gay EXIT
+exit
+
+else
+echo "ERROR ATAU ANDA SALAH MENGENTIKAN ANGKA"
+echo "mohon ulang"
+echo $ulang
+sleep 2
+fi
+done
+
